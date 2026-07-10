b2054e2f6d25e86bacad1e68124d7203e42c215b
Thuc Nguyen <thucnguyen1952000@gmail.com>
2026-07-10 00:55:52 -0700

    style: update PostHog init formatting

--- src/posthog.ts
+++ src/posthog.ts
@@ -1,8 +1,6 @@
 import posthog from 'posthog-js'
 
 posthog.init('phc_nwYYY67qyAj6LBSNHarDi6HeD5tgyfESuyV3taTRjfHA', {
-  api_host: 'https://us.i.posthog.com',
-  defaults: '2026-05-30',
+    api_host: 'https://us.i.posthog.com',
+    defaults: '2026-05-30'
 })
-
-export default posthog
